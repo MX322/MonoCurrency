c3e061de8e2031aa2545d11e569a4395d16c0410
MX322 <mlemix32@gmail.com>
2023-10-24 10:15:06 +0300

    MonoCurrency

---
+++ Currency.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoCurrency
+{
+    internal class Currency
+    {
+        public int currencyCodeA { get; set; }
+        public int currencyCodeB { get; set; }
+        public int date { get; set; }
+        public float rateBuy { get; set; }
+        public float rateCross { get; set; }
+        public float rateSell { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace MonoCurrency
+{
+    internal class Program
+    {
+
+        static async Task<List<Currency>> GetData()
+        {
+            string monoAPI = @"https://api.monobank.ua/bank/currency";
+
+            HttpClient client = new HttpClient();
+
+            var data = await client.GetAsync(monoAPI);
+            string content = await data.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<List<Currency>>(content);
+        }
+
+        static void GetCurrency()
+        {
+            Console.WriteLine($"Date: {DateTime.Today}");
+            foreach (var currency in GetData().Result)
+            {
+                if (currency.currencyCodeA == 840)
+                {
+                    Console.WriteLine($"USD\nBuy: {currency.rateBuy}\nSell: {currency.rateSell}\n");
+                }
+            }
+
+            foreach (var currency in GetData().Result)
+            {
+                if (currency.currencyCodeA == 978)
+                {
+                    Console.WriteLine($"EUR\nBuy: {currency.rateBuy}\nSell: {currency.rateSell}");
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            GetCurrency();
+        }
+    }
+}
